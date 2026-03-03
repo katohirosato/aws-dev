14f0c2fed9ec805b706c6507608db8cae145588a
加藤宏怜 <hirosato654@gmail.com>
2026-03-03 08:14:41 +0000

    github workflows for new repository

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 curl -L -o .devcontainer.json https://raw.githubusercontent.com/katohirosato/aws-dev/main/.devcontainer.json
-# > Dev Container: Rebuild and Reopen in Container as .devcontainer.json
-# > Dev Container: Rebuild and Reopen in Container as .devcontainer/devcontainer.json
 REPONAME=$(basename $(git rev-parse --show-toplevel))
 git init
 git branch -M main
 git remote add origin https://github.com/<repo-owner>/${REPONAME}.git
-git config user.name "Your Name"
-git config user.email "your.email@example.com"
+git config user.name "<Your Name>"
+git config user.email "<your.email@example.com>"
+# > Dev Container: Rebuild and Reopen in Container as .devcontainer.json
+git add .
+git commit -m "first commit"
+git push -u origin main
+
+# > Dev Container: Rebuild and Reopen in Container as .devcontainer/devcontainer.json
