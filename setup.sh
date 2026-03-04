a0cb3ae8674c32d3a0566ec04c502a6ec4243ccc
加藤宏怜 <hirosato654@gmail.com>
2026-03-04 11:06:51 +0000

    fix

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ REPONAME=${1:-aws-app}
 mkdir -p ${REPONAME}
 cd ${REPONAME}
 curl -L -o .devcontainer.json https://raw.githubusercontent.com/katohirosato/aws-dev/main/.devcontainer.json
-# > Dev Container: Rebuild and Reopen in Container as .devcontainer.json
+# > Dev Containers: Rebuild and Reopen in Container as .devcontainer.json
 git init
 git branch -M main
 git remote add origin https://github.com/<repo-owner>/${REPONAME}.git
@@ -12,7 +12,7 @@ git config user.email "<your.email@example.com>"
 git add .
 git commit -m "first commit"
 git push -u origin main
-# > Dev Container: Reopen in WSL
-# > Dev Container: Rebuild and Reopen in Container as .devcontainer/devcontainer.json
+# > Dev Containers: Reopen Folder in WSL
+# > Dev Containers: Rebuild and Reopen in Container as .devcontainer/devcontainer.json
 aws configure sso
 aws sso login
