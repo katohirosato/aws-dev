9af7096be589f4e64a2f78289628860765164672
加藤宏怜 <hirosato654@gmail.com>
2026-03-04 10:24:17 +0000

    update .devcontainer.json

--- setup.sh
+++ setup.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 curl -L -o .devcontainer.json https://raw.githubusercontent.com/katohirosato/aws-dev/main/.devcontainer.json
-REPONAME=$(basename $(git rev-parse --show-toplevel))
+# > Dev Container: Rebuild and Reopen in Container as .devcontainer.json
 git init
 git branch -M main
+REPONAME=$(basename $(git rev-parse --show-toplevel))
 git remote add origin https://github.com/<repo-owner>/${REPONAME}.git
 git config user.name "<Your Name>"
 git config user.email "<your.email@example.com>"
-# > Dev Container: Rebuild and Reopen in Container as .devcontainer.json
 git add .
 git commit -m "first commit"
 git push -u origin main
-
 # > Dev Container: Rebuild and Reopen in Container as .devcontainer/devcontainer.json
