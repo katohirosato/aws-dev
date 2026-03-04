61692dace43f747562e6525ef7a91856bb084bbb
加藤宏怜 <hirosato654@gmail.com>
2026-03-04 10:26:17 +0000

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+REPONAME=${1:-aws-app}
+mkdir -p ${REPONAME}
+cd ${REPONAME}
 curl -L -o .devcontainer.json https://raw.githubusercontent.com/katohirosato/aws-dev/main/.devcontainer.json
 # > Dev Container: Rebuild and Reopen in Container as .devcontainer.json
 git init
 git branch -M main
-REPONAME=$(basename $(git rev-parse --show-toplevel))
 git remote add origin https://github.com/<repo-owner>/${REPONAME}.git
 git config user.name "<Your Name>"
 git config user.email "<your.email@example.com>"
