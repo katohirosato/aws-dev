1678dca028f8c89634de3292cf1d63e6fe990cc5
加藤宏怜 <hirosato654@gmail.com>
2026-04-25 15:44:15 +0900

    Configure Git user name and email in postCreateCommand
    Added Git configuration for user name and email.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,3 +4,5 @@ npm install --global aws-cdk;
 pip install boto3[crt];
 uv python install 3.10;
 curl -fsSL https://cli.kiro.dev/install | bash;
+git config --global user.name katohirosato;
+git config --global user.email hirosato654@gmail.com;
