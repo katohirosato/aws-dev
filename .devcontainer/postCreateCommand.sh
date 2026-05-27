a7805772fbc595c367707c79368bdcb59aafe118
katohirosato <hirosato654@gmail.com>
2026-05-27 04:03:03 +0000

    fix: update directory variable for npm install in postCreateCommand and postStartCommand scripts

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 npm install -g @devcontainers/cli
 npm install --global aws-cdk;
-npm ci;
+cd "${APP}" && npm ci && cd - ;
 pip install boto3[crt];
 uv python install 3.10;
 curl -fsSL https://cli.kiro.dev/install | bash;
--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd "${APP_PROJECT}" && npm ci && cd - ;
+cd "${APP}" && npm ci && cd - ;
 devcontainer templates apply --template-id ghcr.io/katohirosato/aws-dev/aws-dev --omit-paths '[".git/*","README.md",".github/workflows/template.yaml","devcontainer-template.json","history.sh", "startup.sh", ".devcontainer/config", ".devcontainer/.env"]';
 git config --global user.name katohirosato
 git config --global user.email hirosato654@gmail.com
