a9af03d05f0078f423b0cbccd874a5b86dc3d4b7
katohirosato <hirosato654@gmail.com>
2026-07-06 17:04:45 +0000

    chore: format postStartCommand.sh for improved readability

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-devcontainer templates apply --template-id ghcr.io/katohirosato/aws-dev/aws-dev --omit-paths '[".git/*","README.md",".github/workflows/template.yaml","devcontainer-template.json","history.sh", "startup.sh", ".devcontainer/config", ".devcontainer/.env", "cdk-elements/*"]';
+devcontainer templates apply \
+  --template-id ghcr.io/katohirosato/aws-dev/aws-dev \
+  --omit-paths '[
+    ".git/*",
+    ".gitignore",
+    ".github/workflows/template.yaml",
+    ".devcontainer/config",
+    ".devcontainer/.env",
+    "devcontainer-template.json",
+    "history.sh",
+    "startup.sh",
+    "README.md",
+    "cdk-elements/*"
+  ]';
 git config --global user.name katohirosato
 git config --global user.email hirosato654@gmail.com
